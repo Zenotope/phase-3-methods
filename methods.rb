99decb6ab7b1c954aa7fbb4f44b4beef9b592f53
Zenotope <zaak.kerstetter@gmail.com>
2022-06-28 09:26:52 -0600

    commit tree 615793068f317a7fcd300becfd9565646d4bba12

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name) 
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (a = 1, b= 4)
+    return a + b
+end
+
+def halve (num)
+    return nil unless num.class == Integer
+    num / 2
+end
